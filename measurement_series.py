d64f8e4e02e46536e837d63c52423e628dcc04b2
Benjamin Haubmann <ge23jud@mytum.de>
2025-08-08 15:17:09 +0200

    Added empty class MeasurementSeries()

---
+++ measurement_series.py
@@ -0,0 +1,16 @@
+from helper_functions import HelperFunctions
+
+class MeasurementSeries():
+
+    def __init__(self, data, filepath):
+        """
+        Series of m measurements with n_m data points, respectively. n_m can be different for every single measurement.
+        Every measurement itself is of type Measurement(). The list of measurements read from file.
+
+        Parameters:
+        data (func): function to be used to load data. Returns
+        filepath (str): Filepath of the file from which data is loaded
+        """
+
+        self.info = None # read from file or passed in form of
+        self.
