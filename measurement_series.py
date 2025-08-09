3fca5a3165ded9f0a97d0a6657a55a7594b0d414
Benjamin Haubmann <ge23jud@mytum.de>
2025-08-09 16:13:56 +0200

    Added function load() to MeasurementSeries()

--- measurement_series.py
+++ measurement_series.py
@@ -11,6 +11,16 @@ class MeasurementSeries():
         data (func): function to be used to load data. Returns
         filepath (str): Filepath of the file from which data is loaded
         """
+        self.info, self.X, self.Y = self.load()
 
-        self.info = None # read from file or passed in form of
-        self.
+
+    def load(self, data, filepath):
+        """
+        Series of m measurements with n_m data points, respectively. n_m can be different for every single measurement.
+        Every measurement itself is of type Measurement(). The list of measurements read from file.
+
+        Parameters:
+        data (func): function to be used to load data. Takes filepath as an argument and returns tuple (dic, array (n,n), array (n,n))
+        filepath (str): Filepath of the file from which data is loaded
+        """
+        return data(filepath)
